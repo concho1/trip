57ff3312fe3e4292f660e2ff17be5053a960e87c
goott3 <eomseunghyeon480@gmail.com>
2024-06-13 17:58:08 +0900

    gg

--- src/main/java/com/goott/trip/TripApplication.java
+++ src/main/java/com/goott/trip/TripApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TripApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(TripApplication.class, args);
+    public static void main(String[] args) { SpringApplication.run(TripApplication.class, args);
     }
-
 }
