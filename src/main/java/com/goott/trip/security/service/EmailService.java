cc79266c955a539cd0f9623bd14b90e06acdcba5
ming <01041463156a@gamil.com>
2024-06-14 18:19:27 +0900

    이메일 인증

--- src/main/java/com/goott/trip/security/service/EmailService.java
+++ src/main/java/com/goott/trip/security/service/EmailService.java
@@ -1,5 +1,9 @@
 package com.goott.trip.security.service;
 
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -13,27 +17,61 @@ public class EmailService {
 
     public  static final String code = createcode();
 
+    public String sendEmail(String email, HttpSession session) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.addRecipients(Message.RecipientType.TO,email);
+        message.setSubject("[tripHamsic] 본인확인 인증 코드입니다.");
+
+        String msgg="";
+        msgg+= "<div style='margin:100px;'>";
+        msgg+= "<h1> 안녕하세요 </h1>";
+        msgg+= "<br>";
+        msgg+= "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
+        msgg+= "<br>";
+        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg+= "<h3 style='color:blue;'>회원가입 코드입니다.</h3>";
+        msgg+= "<div style='font-size:130%'>";
+        msgg+= "CODE : <strong>";
+        msgg+= code + "</strong><div><br/> ";
+        msgg+= "</div>";
+        session.setAttribute("sCode", code);
+        message.setText(msgg,"UTF-8","HTML");
+        javaMailSender.send(message);
+
+        return msgg;
+    }
+
+    public String sendPwdEmail(String email, HttpSession session) throws MessagingException {
+        System.out.println(email);
+        MimeMessage message = javaMailSender.createMimeMessage();
+        message.addRecipients(Message.RecipientType.TO,email);
+        message.setSubject("[tripHamsic] 비밀번호 찾기 인증 코드입니다.");
+
+        String msgg="";
+        msgg+= "<div style='margin:100px;'>";
+        msgg+= "<h1>  </h1>";
+        msgg+= "<br>";
+        msgg+= "<p>아래 코드를 회원가입 창으로 돌아가 입력해주세요<p>";
+        msgg+= "<br>";
+        msgg+= "<div align='center' style='border:1px solid black; font-family:verdana';>";
+        msgg+= "<h3 style='color:blue;'>비밀번호찾기 코드입니다.</h3>";
+        msgg+= "<div style='font-size:130%'>";
+        msgg+= "CODE : <strong>";
+        msgg+= code + "</strong><div><br/> ";
+        msgg+= "</div>";
+        session.setAttribute("sCode", code);
+        message.setText(msgg,"UTF-8","HTML");
+        javaMailSender.send(message);
+
+        return msgg;
+    }
+
     public static String createcode() {
         StringBuffer key = new StringBuffer();
         Random rnd = new Random();
 
-        for (int i = 0; i < 8; i++) {
-            int index = rnd.nextInt(3); // 0~2 까지 랜덤
-
-            switch (index) {
-                case 0:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 97));
-                    //  a~z  (ex. 1+97=98 => (char)98 = 'b')
-                    break;
-                case 1:
-                    key.append((char) ((int) (rnd.nextInt(26)) + 65));
-                    //  A~Z
-                    break;
-                case 2:
-                    key.append((rnd.nextInt(10)));
-                    // 0~9
-                    break;
-            }
+        for (int i = 0; i < 4; i++) {
+            key.append(rnd.nextInt(10));
         }
 
         return key.toString();
