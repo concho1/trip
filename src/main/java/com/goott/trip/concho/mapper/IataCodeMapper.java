ef535fd96fd96110031ba546ec754e7c1b7e9915
concho1 <142205346+concho1@users.noreply.github.com>
2024-06-20 11:07:19 +0900

    concho

--- src/main/java/com/goott/trip/concho/mapper/IataCodeMapper.java
+++ src/main/java/com/goott/trip/concho/mapper/IataCodeMapper.java
@@ -1,12 +1,19 @@
 package com.goott.trip.concho.mapper;
 
+import com.goott.trip.concho.model.IataCode;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 @Mapper
 public interface IataCodeMapper {
     void insertIataCode(@Param("iataCode") String iataCode,
                         @Param("city") String city,
+                        @Param("cityKo") String cityKo,
                         @Param("country") String country,
                         @Param("countryKo") String countryKo);
+
+    Optional<IataCode> findCityByIataCode(@Param("iataCode") String iataCode);
 }
