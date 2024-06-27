183b4c8222b7c3d4b3f6053e6210d8d2e6fe310d
Taebae00 <teagun70000@naver.com>
2024-06-27 16:09:17 +0900

    햄스터일함

---
+++ src/main/java/com/goott/trip/hamster/model/Hotel.java
@@ -0,0 +1,12 @@
+package com.goott.trip.hamster.model;
+
+import lombok.Data;
+
+@Data
+public class Hotel {
+
+    private String hotelIdKey;
+    private String hotelContKey;
+    private String crImgKey;
+
+}
