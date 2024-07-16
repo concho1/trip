372583b02200db2659d53bad8cccf7f4fcf441e9
Taebae00 <teagun70000@naver.com>
2024-07-16 11:57:13 +0900

    장바구니 와장창공사중

--- src/main/resources/static/ham.js/DeleteButton.js
+++ src/main/resources/static/ham.js/DeleteButton.js
@@ -1,9 +1,36 @@
-function deleteItem(num) {
+function deleteAir(num) {
 
-    var hotelKeyVal = $('#hotelKey_'+num).text().trim();
+    var AirKeyVal = $('#hotelKey_'+num).val();
+
+    alert(AirKeyVal);
+
+    /*$.ajax({
+        url: '/member/hamster/airplane/cartHotelDelete',
+        method: 'POST',
+        data: { hotelKeyVal: hotelKeyVal},
+        dataType: 'json',
+        success: function(data) {
+            if (data.success) {
+                $('#hotelList').find('#hotelKey_'+num).closest('.myedu-area').remove();
+            } else {
+                alert('장바구니 삭제 중 오류 발생' + data.message);
+            }
+        },
+        error: function(xhr, status, error) {
+            console.error(error);
+            alert('AJAX 오류 발생: ' + error);
+        }
+    });*/
+}
+
+function deleteHotel(num) {
+
+    var hotelKeyVal = $('#hotelKey_'+num).val();
+
+    alert(hotelKeyVal)
 
     $.ajax({
-        url: '/member/hamster/airplane/categoryDelete',
+        url: '/member/hamster/airplane/cartHotelDelete',
         method: 'POST',
         data: { hotelKeyVal: hotelKeyVal},
         dataType: 'json',
