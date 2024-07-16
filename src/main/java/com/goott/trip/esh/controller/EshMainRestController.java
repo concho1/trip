5e092fff3534b219a5beef2a2cdd145044b77eff
goott3 <eomseunghyeon480@gmail.com>
2024-07-16 18:19:45 +0900

    gg

--- src/main/java/com/goott/trip/esh/controller/EshMainRestController.java
+++ src/main/java/com/goott/trip/esh/controller/EshMainRestController.java
@@ -2,10 +2,15 @@ package com.goott.trip.esh.controller;
 
 import com.goott.trip.esh.service.ExchangeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("user/esh")
@@ -13,11 +18,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class EshMainRestController {
     private final ExchangeService exchangeService;
 
-    @GetMapping("/do-convert")
-    public String convertCurrency(@RequestParam("fromCurrency") String fromCurrency,
-                                  @RequestParam("toCurrency") String toCurrency,
-                                  @RequestParam("amount") double amount) {
-        double convertedAmount = exchangeService.convertCurrency(fromCurrency, toCurrency, amount);
-        return String.valueOf(convertedAmount); // 결과를 문자열로 반환
+    @GetMapping("/exchange-rate")
+    public String exchangeData(Model model) {
+        List<Map<String, Object>> exchangeRates = exchangeService.getExchangeData();
+        model.addAttribute("exchangeRates", exchangeRates);
+        System.out.println(exchangeRates);
+        return String.valueOf(exchangeRates);
+    }
+
+    @PostMapping("/convert-currency")
+    public double convertCurrency(@RequestParam("fromCurrency") String fromCurrency) {
+        if(fromCurrency.equalsIgnoreCase("JPY") || fromCurrency.equalsIgnoreCase("IDR")) {
+            return Math.round(Double.parseDouble(this.exchangeService.getExchageRate(fromCurrency))/100.0);
+        }
+        return Math.round(Double.parseDouble(this.exchangeService.getExchageRate(fromCurrency)));
+
     }
 }
