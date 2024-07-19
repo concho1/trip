df0b02f9f131949fa2ba4aa42b7669bf5f0f7891
Taebae00 <teagun70000@naver.com>
2024-07-19 17:20:03 +0900

    이준우는 신이다

--- src/main/java/com/goott/trip/hamster/model/Payment.java
+++ src/main/java/com/goott/trip/hamster/model/Payment.java
@@ -52,14 +52,23 @@ public class Payment {
     private String ridePassportCountryStr;
     private String ridePassportExdateStr;
 
-    public void AllArrayToStr(){
-        rideFirstNameStr = String.join(",",this.rideFirstName);
-        rideLastNameStr = String.join(",",this.rideLastName);
-        rideBirthStr = String.join(",",this.rideBirth);
-        rideCountryStr = String.join(",",this.rideCountry);
-        ridePassportStr = String.join(",",this.ridePassport);
-        ridePassportCountryStr = String.join(",",this.ridePassportCountry);
-        ridePassportExdateStr = String.join(",",this.ridePassportExdate);
+    public void AllArrayToStr() {
+        if (this.rideFirstName != null) rideFirstNameStr = String.join(",", this.rideFirstName);
+        if (this.rideLastName != null) rideLastNameStr = String.join(",", this.rideLastName);
+        if (this.rideBirth != null) rideBirthStr = String.join(",", this.rideBirth);
+        if (this.rideCountry != null) rideCountryStr = String.join(",", this.rideCountry);
+        if (this.ridePassport != null) ridePassportStr = String.join(",", this.ridePassport);
+        if (this.ridePassportCountry != null) ridePassportCountryStr = String.join(",", this.ridePassportCountry);
+        if (this.ridePassportExdate != null) ridePassportExdateStr = String.join(",", this.ridePassportExdate);
+    }
 
+    public void AllStrToArray() {
+        if (this.rideFirstNameStr != null) this.rideFirstName = this.rideFirstNameStr.split(",");
+        if (this.rideLastNameStr != null) this.rideLastName = this.rideLastNameStr.split(",");
+        if (this.rideBirthStr != null) this.rideBirth = this.rideBirthStr.split(",");
+        if (this.rideCountryStr != null) this.rideCountry = this.rideCountryStr.split(",");
+        if (this.ridePassportStr != null) this.ridePassport = this.ridePassportStr.split(",");
+        if (this.ridePassportCountryStr != null) this.ridePassportCountry = this.ridePassportCountryStr.split(",");
+        if (this.ridePassportExdateStr != null) this.ridePassportExdate = this.ridePassportExdateStr.split(",");
     }
 }
